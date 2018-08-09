f8282ce617ff8b210d51cb801f84f88922d7736a
achasik <achasik@gmail.com>
2018-08-09 16:15:44 +0400

    detail

--- routes/detail.js
+++ routes/detail.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = require("express-promise-router")();
-const DetailController = require("../controllers/details");
+const DetailController = require("../controllers/detail");
 
 router.route("/:id").get(DetailController.film);
 
