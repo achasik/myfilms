c275e41b8bdd26ef5ff2923d7b8160b9636e340d
achasik <achasik@gmail.com>
2019-05-03 14:41:23 +0400

    api search

--- controllers/films.js
+++ controllers/films.js
@@ -5,18 +5,35 @@ const Torrent = require("../models/torrent");
 module.exports = {
   index: async (req, res) => {
     const page = req.query.page || 1;
+    const search = req.query.search;
     //const page = parseInt(pageStr);
-    const films = await Film.find(
-      {
-        seen: { $ne: true },
-        torrents: { $gt: [] } //,
-        // updatedAt: { $gt: new Date(new Date() - 240 * 60 * 60 * 1000) }
-      },
-      { __v: 0 }
-    )
-      .sort({ year: -1, updatedAt: -1 })
-      .skip((page - 1) * 50)
-      .limit(50);
+    let films;
+    if (search) {
+      films = await Film.find(
+        {
+          $or: [
+            { name: { $regex: search, $options: "i" } },
+            { nameRu: { $regex: search, $options: "i" } }
+          ],
+          torrents: { $gt: [] } //,
+          // updatedAt: { $gt: new Date(new Date() - 240 * 60 * 60 * 1000) }
+        },
+        { __v: 0 }
+      ).sort({ year: -1, updatedAt: -1 });
+    } else {
+      films = await Film.find(
+        {
+          seen: { $ne: true },
+          torrents: { $gt: [] } //,
+          // updatedAt: { $gt: new Date(new Date() - 240 * 60 * 60 * 1000) }
+        },
+        { __v: 0 }
+      )
+        .sort({ year: -1, updatedAt: -1 })
+        .skip((page - 1) * 50)
+        .limit(50);
+    }
+
     res.status(200).json(films);
   },
   film: async (req, res) => {
